59dd031995d907fc02f14d590041a19327e0bccd
Yash Shukla <yashshukla1912@gmail.com>
2020-06-23 18:37:46 +0530

    Added code to send mail to requester in normal task and Email task

--- src/Notifications.php
+++ src/Notifications.php
@@ -5,6 +5,7 @@ namespace ProcessMaker\Packages\Connectors\Email;
 use Illuminate\Support\Facades\Log;
 use ProcessMaker\Facades\WorkflowManager;
 use ProcessMaker\Models\Process;
+use ProcessMaker\Models\ProcessRequest;
 use ProcessMaker\Nayra\Bpmn\Events\ActivityActivatedEvent;
 use ProcessMaker\Nayra\Bpmn\Events\ActivityCompletedEvent;
 use ProcessMaker\Packages\Connectors\Email\Seeds\EmailSendSeeder;
@@ -43,40 +44,37 @@ class Notifications
      */
     private function sendNotificationAt($event, $sendAt)
     {
-$token = $event->token;
-$user_id = $token->user ? $token->user_id : null;
-Log::info("*************Token*************");
-Log::info($token);
-// Log::info($token->processRequest->data);
+
         if (!isset($event->token->getDefinition()['config'])) {
             return;
         }
         $config = json_decode($event->token->getDefinition()['config'], true);
-// Log::info("*************Config*************");
-// Log::info($config);
 
         if (isset($config['email_notifications'])) {
             foreach ($config['email_notifications']['notifications'] as $notificationConfig) {
                 if ($notificationConfig['sendAt'] !== $sendAt) {
                     continue;
                 }
-                if ($notificationConfig['sendToAssignee']) {
-Log::info("*************before*************");
-Log::info($notificationConfig['users']);
+                // Send mail to assignee(Normal Task)
+                if (isset($notificationConfig['sendToAssignee']) && $notificationConfig['sendToAssignee']) {
                     if (!in_array($event->token->user_id, $notificationConfig['users'])) {
                         array_push($notificationConfig['users'], $event->token->user_id);
                     }
-                    // $notificationConfig['users'] = in_array($event->token->user_id, $notificationConfig['users']) ? $notificationConfig['users'] : array_push($notificationConfig['users'], $event->token->user_id);
-Log::info("*************after*************");
-Log::info($notificationConfig['users']);
+                }
+                // Send mail to requester(Normal Task)
+                if (isset($notificationConfig['sendToRequester']) && $notificationConfig['sendToRequester']) {
+                    if (isset($event->token->process_request_id)) {
+                        $data = ProcessRequest::where('id','=', $event->token->process_request_id)->first();
+                        if (!in_array($data["user_id"], $notificationConfig['users'])) {
+                            array_push($notificationConfig['users'], $data["user_id"]);
+                        }
+                    }
                 }
                 $this->createNotification(
                     $notificationConfig,
                     $event->token
                 );
             }
-Log::info("*************Config*************");
-Log::info($config);
         }
     }
 
@@ -89,9 +87,9 @@ Log::info($config);
     private function createNotification($notificationConfig, $token)
     {
         $subProcess = $this->notificationSubProcess();
-// print("<pre>".print_r($subProcess, true)."</pre>");
+
         $definitions = $subProcess->getDefinitions();
-// print("<pre>".print_r($definitions, true)."</pre>");exit;
+
         $event = $definitions->getEvent(EmailSendSeeder::SUB_PROCESS_START_EVENT);
         WorkflowManager::triggerStartEvent(
             $subProcess, $event, array_merge($token->processRequest->data, [
